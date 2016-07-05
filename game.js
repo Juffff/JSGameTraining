4fe54ec7e18eecf571ea5e27d3886dbdab81c10c
Juff <vladimir.voytsekhovskiy@gmail.com>
2016-07-05 23:47:04 +0300

    cycle

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 window.onload = init;
 
-var  map;
+var map;
 var ctxMap;
 
 var player;
 var ctxPlayer;
+
+var enemy;
+var ctxEnemy;
 
 var drawButton;
 var clearButton;
@@ -18,6 +21,41 @@ background.src = "img/bg.jpg";
 var tiles = new Image();
 tiles.src = "img/tiles.png";
 
+var requestAnimFrame = window.requestAnimFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+
+var isPlaying;
+
+function Player() {
+    this.srcX = 0;
+    this.srcY = 0;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.weight = 150;
+    this.height = 110;
+    
+    this.speed = 5;
+}
+
+Player.prototype.draw = function () {
+    ctxMap.drawImage(tiles,this.srcX,this.srcY,this.weight,this.height,this.drawX,this.drawY,this.weight,this.height);
+};
+
+function Enemy() {
+
+    this.srcX = 0;
+    this.srcY = 110;
+    this.drawX = 500;
+    this.drawY = 30;
+    this.width = 150;
+    this.height = 110;
+
+    this.speed = 8;
+}
+
+Enemy.prototype.draw = function(){
+    ctxMap.drawImage(tiles, this.srcX,this.srcY,this.width, this.height, this.drawX,this.drawY,this.width, this.height);
+};
+
 
 function init() {
     map = document.getElementById("map");
@@ -29,15 +67,52 @@ function init() {
     ctxPlayer = player.getContext("2d");
     player.width = gameWidth;
     player.height = gameHeight;
+    
+    enemy = document.getElementById("enemy");
+    ctxEnemy = enemy.getContext("2d");
+    enemy.height = gameHeight;
+    enemy.width = gameWidth;
 
     drawButton = document.getElementById("drawButton");
     clearButton = document.getElementById("clearButton");
     drawButton.addEventListener("click", drawRect, false);
     clearButton.addEventListener("click", clearRect, false);
     drawBg();
-    drawPlayer();
+    player = new Player();
+    enemy = new Enemy();
+    startLoop();
 
 }
+
+function draw () {
+    player.draw();
+    enemy.draw();
+}
+
+function update() {
+
+}
+
+function loop() {
+
+    if(isPlaying){
+        draw();
+        update();
+        requestAnimFrame(loop);
+    }
+
+}
+
+function startLoop() {
+    isPlaying = true;
+    loop();
+}
+
+function stopLoop() {
+    isPlaying = false;
+}
+
+
 
 function drawRect() {
     ctxMap.fillStyle = "#3d3d3d";
@@ -52,7 +127,4 @@ function drawBg() {
     ctxMap.drawImage(background,0,0,800,500,0,0,800,500);
 }
 
-function drawPlayer() {
-    ctxMap.drawImage(tiles,0,0,150,130,0,0,150,130);
-}
 
