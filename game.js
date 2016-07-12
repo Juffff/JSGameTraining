a917832e8c530c6ab696cf31369481972635d9f1
Juff <vladimir.voytsekhovskiy@gmail.com>
2016-07-12 17:36:53 +0300

    dir, random

--- game.js
+++ game.js
@@ -31,37 +31,113 @@ function Player() {
     this.height = 110;
 
     this.speed = 5;
+
+    //For keys
+    this.isUp = false;
+    this.isDown = false;
+    this.isLeft = false;
+    this.isRight = false;
+
 }
 
 Player.prototype.draw = function () {
     clearCtxPlayer();
-    ctxPlayer.drawImage(tiles,this.srcX,this.srcY,this.weight,this.height,this.drawX,this.drawY,this.weight,this.height);
+    ctxPlayer.drawImage(tiles, this.srcX, this.srcY, this.weight, this.height, this.drawX, this.drawY, this.weight, this.height);
 };
 
-Player.prototype.clear = function () {
-    ctxPlayer.clear();
-};
+Player.prototype.chooseDirection = function () {
+    if (this.isUp) {
+        this.drawY -= this.speed;
+    }
+
+    if (this.isDown) {
+        this.drawY += this.speed;
+    }
+
+    if (this.isLeft) {
+        this.drawX -= this.speed;
+    }
+
+    if (this.isRight) {
+        this.drawX += this.speed;
+    }
+}
 
 Player.prototype.update = function () {
-     this.drawX +=this.speed;
-
+    this.chooseDirection();
 };
 
+function checkKeyDown(e) {
+    var keyID = e.keyCode || e.which;
+    var keyChar = String.fromCharCode(keyID);
+    if(keyChar=="W"){
+        player.isUp=true;
+        e.preventDefault();
+    }
+
+    if(keyChar=="S"){
+        player.isDown=true;
+        e.preventDefault();
+    }
+
+    if(keyChar=="A"){
+        player.isLeft=true;
+        e.preventDefault();
+    }
+
+    if(keyChar=="D"){
+        player.isRight=true;
+        e.preventDefault();
+    }
+}
+
+function checkKeyUp(e) {
+    var keyID = e.keyCode || e.which;
+    var keyChar = String.fromCharCode(keyID);
+    if(keyChar=="W"){
+        player.isUp=false;
+        e.preventDefault();
+    }
+
+    if(keyChar=="S"){
+        player.isDown=false;
+        e.preventDefault();
+    }
+
+    if(keyChar=="A"){
+        player.isLeft=false;
+        e.preventDefault();
+    }
+
+    if(keyChar=="D"){
+        player.isRight=false;
+        e.preventDefault();
+    }
+}
 
 function Enemy() {
 
     this.srcX = 0;
     this.srcY = 110;
-    this.drawX = 500;
-    this.drawY = 30;
+    this.drawX = gameWidth+ Math.floor(Math.random()*10);
+    this.drawY = Math.floor(Math.random()*gameHeight);
     this.width = 150;
     this.height = 110;
 
-    this.speed = 8;
+    this.speed = 2;
 }
 
-Enemy.prototype.draw = function(){
-    ctxEnemy.drawImage(tiles, this.srcX,this.srcY,this.width, this.height, this.drawX,this.drawY,this.width, this.height);
+Enemy.prototype.draw = function () {
+    clearCtxEnemy();
+    ctxEnemy.drawImage(tiles, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
+Enemy.prototype.update = function () {
+    this.drawX-=this.speed;
+};
+
+Enemy.prototype.clear = function () {
+    ctxEnemy.clear();
 };
 
 
@@ -70,24 +146,29 @@ function init() {
     ctxMap = map.getContext("2d");
     map.width = gameWidth;
     map.height = gameHeight;
-    
+
     player = document.getElementById("player");
     ctxPlayer = player.getContext("2d");
     player.width = gameWidth;
     player.height = gameHeight;
-    
+
     enemy = document.getElementById("enemy");
     ctxEnemy = enemy.getContext("2d");
     enemy.height = gameHeight;
     enemy.width = gameWidth;
     drawBg();
+
+
     player = new Player();
     enemy = new Enemy();
     startLoop();
 
+    document.addEventListener("keydown", checkKeyDown, false);
+    document.addEventListener("keyup", checkKeyUp, false);
+
 }
 
-function draw () {
+function draw() {
     player.draw();
 
     enemy.draw();
@@ -95,11 +176,12 @@ function draw () {
 
 function update() {
     player.update();
+    enemy.update();
 }
 
 function loop() {
 
-    if(isPlaying){
+    if (isPlaying) {
         draw();
         update();
         requestAnimFrame(loop);
@@ -117,10 +199,15 @@ function stopLoop() {
 }
 
 function drawBg() {
-    ctxMap.drawImage(background,0,0,gameWidth,gameHeight,0,0,gameWidth,gameHeight);
+    ctxMap.drawImage(background, 0, 0, gameWidth, gameHeight, 0, 0, gameWidth, gameHeight);
 }
 
 function clearCtxPlayer() {
-    ctxPlayer.clearRect(0,0,gameWidth,gameHeight);
+    ctxPlayer.clearRect(0, 0, gameWidth, gameHeight);
+}
+
+
+function clearCtxEnemy() {
+    ctxEnemy.clearRect(0, 0, gameWidth, gameHeight);
 }
 
